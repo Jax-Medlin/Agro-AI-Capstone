e7aca5c2a7f798e150ca498290c6ec5e82bc1fd6
MajorTripp <jeradtripp@gmail.com>
2024-04-18 20:36:15 -0500

    databasefixes

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,16 @@
 from flask import Flask
-from config import Config
 from flask_mysqldb import MySQL
-import MySQLdb.cursors
-import MySQLdb.cursors, re, hashlib
+from config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
 mysql = MySQL(app)
 
-try:
-    connection = mysql.connect()
-    print("MySQL connection:", connection)
-except Exception as e:
-    print("Error connecting to MySQL:", e)
+with app.app_context():
+    try:
+        connection = mysql.connection
+        print("MySQL connection:", connection)
+    except Exception as e:
+        print("Error connecting to MySQL:", e)
 
 from app import web
